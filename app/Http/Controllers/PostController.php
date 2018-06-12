be99a65a11dbcf991ecc11d7936f89b53b1f08a7
Daniel Supernault <danielsupernault@gmail.com>
2018-06-12 00:03:38 -0600

    Update PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function home()
+    {
+        $posts = Post::whereStatus('PUBLISHED')->orderBy('created_at', 'desc')->paginate(10);
+        return view('blog.home', compact('posts'));
+    }
+
+    public function show(Request $request, $year, $month, $slug)
+    {
+        $post = Post::whereSlug($slug)->firstOrFail();
+        return view('blog.post.show', compact('post'));
+    }
 }
