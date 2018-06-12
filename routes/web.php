4876695c4a8d7682323e1eee86faf9c6daab4673
Daniel Supernault <danielsupernault@gmail.com>
2018-06-11 22:57:02 -0600

    Add Voyager assets

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
