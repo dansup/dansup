48ce36c5c1f8f9995d0a0e7af88cff821d3ded49
Daniel Supernault <danielsupernault@gmail.com>
2018-06-12 20:14:54 -0600

    Add highlight.js

--- resources/views/blog/post/show.blade.php
+++ resources/views/blog/post/show.blade.php
@@ -1,8 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="container mt-5 py-5">
+<div class="container">
+    <div class="col-12 col-md-8 offset-md-2 pt-3">
+        <a href="/" class="font-weight-bold">dansup</a>
+        <span class="px-2 text-muted">/</span>
+        <a href="/blog" class="font-weight-bold">blog</a>
+    </div>
+</div>
+<div class="container mt-5">
   
     <div class="col-12 col-md-8 offset-md-2 blog-post">
         <div class="pt-3">
@@ -27,3 +33,13 @@
 </div>
 
 @endsection
+
+
+@push('styles')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/dracula.min.css" integrity="sha256-tBn0J8E6kxVtZfyZhjb3ck8M44PMKCeOwhfGXPM20Bw=" crossorigin="anonymous" />
+@endpush
+
+@push('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js" integrity="sha256-/BfiIkHlHoVihZdc6TFuj7MmJ0TWcWsMXkeDFwhi0zw=" crossorigin="anonymous"></script>
+<script>hljs.initHighlightingOnLoad();</script>
+@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,16 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
-
-    <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    @stack('styles')
+
 </head>
 <body>
     <div id="app">
@@ -22,5 +18,8 @@
             @yield('content')
         </main>
     </div>
+
+    @stack('scripts')
+
 </body>
 </html>
