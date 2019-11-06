18217a613dfb94aa7bd11b8311816f1cb2b6efca
Daniel Supernault <danielsupernault@gmail.com>
2019-11-05 22:13:33 -0700

    Clean up deps and webpack config

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,19 +1,7 @@
-window._ = require('lodash');
-window.Popper = require('popper.js').default;
-
 try {
     window.$ = window.jQuery = require('jquery');
-
     require('bootstrap');
 } catch (e) {}
-
-window.axios = require('axios');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-let token = document.head.querySelector('meta[name="csrf-token"]');
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-}
 
 const warningTitleCSS = 'color:red; font-size:60px; font-weight: bold; -webkit-text-stroke: 1px black;';
 const warningDescCSS = 'font-size: 18px;';
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,8 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css" />
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css" /> --}}
     <link href="https://fonts.googleapis.com/css?family=Overpass:400,700" rel="stylesheet">
-    <script src="{{ mix('js/app.js') }}" defer></script>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @stack('styles')
 
@@ -21,6 +20,9 @@
         </main>
     </div>
 
+    <script src="{{ mix('js/manifest.js') }}"></script>
+    <script src="{{ mix('js/vendor.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
     @stack('scripts')
 
 </body>
